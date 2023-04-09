10e5e181af72bf9b168d7906ae66106d5f569a15
durgasri92 <mdurgasri92@gmail.com>
2023-04-09 17:42:19 +0530

    second commit

--- src/test/java/testcases/Requests.java
+++ src/test/java/testcases/Requests.java
@@ -2,9 +2,12 @@ package testcases;
 
 import static io.restassured.RestAssured.given;
 
+import org.apache.log4j.xml.DOMConfigurator;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.response.Response;
+import resources.Log4j;
 
 public class Requests {
 
@@ -13,19 +16,24 @@ public class Requests {
 	@Test(description = "Method to demonstrate get method using path parameters")
 	public void getMethodwithPathParam() {
 
+		DOMConfigurator.configure("log4j.xml");
+		Log4j.startTestCase("getMethodwithPathParam");
 		Response res = given().when().get(baseURL + "/api/users?page=2");
-
-		System.out.println(res.asString());
+		Assert.assertEquals(res.getStatusCode(), 200);
+		Log4j.info(res.asPrettyString());
+		// System.out.println(res.asString());
 		System.out.println(res.asPrettyString());
 
 	}
 
 	@Test
 	public void PostMethod() {
-
+		DOMConfigurator.configure("log4j.xml");
+		Log4j.startTestCase("PostMethod");
 		Response res = given().header("Content-Type", "application/json")
 				.body("{\"name\":\"morpheus\",\"job\":\"leader\"}").when().post("https://reqres.in/api/users");
 		System.out.println(res.asPrettyString());
+		Log4j.info(res.asPrettyString());
 	}
 
 }
